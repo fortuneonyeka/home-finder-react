dfb6387483dfdc20755646d87f154e269f1bbc37
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-06-12 09:00:32 +0100

    create slider initial settings

---
+++ src/utils/common.js
@@ -0,0 +1,18 @@
+export const sliderSettings = {
+  slidePerview: 1,
+  spaceBetween: 5,
+  breakpoints: {
+    480: {
+      slidePerview : 1
+    },
+    600: {
+      slidePerview : 2
+    },
+    750: {
+      slidePerview : 3
+    },
+    1100: {
+      slidePerview : 4
+    }
+  }
+}
