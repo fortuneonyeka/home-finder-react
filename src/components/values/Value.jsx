b2bbec172544cec4deae48914dacd93ddc4209b7
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-06-13 12:13:08 +0100

    implement right and left sides of the value component

--- src/components/values/Value.jsx
+++ src/components/values/Value.jsx
@@ -7,13 +7,28 @@ import {
   AccordionItemPanel,
   AccordionItemState
 } from "react-accessible-accordion"
+import "react-accessible-accordion/dist/fancy-example.css"
 import "./value.css"
+import {MdOutlineArrowDropDown} from "react-icons/md"
 
 const Value = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="v-wrapper">
+      <div className="paddings innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="" />
+          </div>
+        </div>
+        {/* right side */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Core Values</span>
+          <span className="primaryText">What You Get From Us</span>
+          <span className="secondaryText">Letting you find homes with perfection <br /> Hassle-free way to find your dream homes.</span>
+        </div>
+      </div>
+    </section>
   )
 }
 
