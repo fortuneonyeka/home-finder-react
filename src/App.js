43f0dde8de9854af40c2b71b230ed0cb278e4af9
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-06-13 11:50:26 +0100

    setup values component template

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Companies from "./components/companies/Companies";
 import Redidencies from "./components/residencies/Redidencies";
+import Value from "./components/values/Value";
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
         <Hero />
       </div>
         <Companies />
-
         <Redidencies />
+        <Value />
     </div>
   );
 }
---
+++ src/components/values/Value.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState
+} from "react-accessible-accordion"
+import "./value.css"
+
+const Value = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Value
