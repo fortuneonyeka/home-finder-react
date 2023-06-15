d36da108fc42d0df271d6995363d3f986666a54b
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-06-15 09:37:32 +0100

    build the accordion secion

--- src/components/values/Value.jsx
+++ src/components/values/Value.jsx
@@ -10,6 +10,7 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css"
 import "./value.css"
 import {MdOutlineArrowDropDown} from "react-icons/md"
+import accordionData from "../../utils/accordion"
 
 const Value = () => {
   return (
@@ -25,7 +26,27 @@ const Value = () => {
         <div className="flexColStart v-right">
           <span className="orangeText">Our Core Values</span>
           <span className="primaryText">What You Get From Us</span>
-          <span className="secondaryText">Letting you find homes with perfection <br /> Hassle-free way to find your dream homes.</span>
+          <span className="secondaryText">Letting you find homes with perfection. <br /> A hassle-free way to find your dream homes.</span>
+
+          <Accordion
+          className="accordion"
+          allowMultipleExpanded={false}
+          preExpanded={[0]}>
+              {accordionData.map((data, i) => (
+                <AccordionItem className="accordionItem" key={i} uuid={i}>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      <div className="flexCenter icon">{data.icon}</div>
+                      <span className="primaryTex">{data.heading}</span>
+                      <div className="flexCenter icon"><MdOutlineArrowDropDown size={20}/></div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{data.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              ))}
+          </Accordion>
         </div>
       </div>
     </section>
