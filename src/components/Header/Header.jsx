b62fd080b79cf2ed26bb2806652406894457cca4
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-06-23 16:22:09 +0100

    animate the h-menu with transition all ease in and also implement the OutsideClideHandler to close the menu when clicked outside of it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,48 @@
-import React, {useState} from 'react'
-import "./Header.css"
-import { Link } from "react-router-dom"
-import {BiMenuAltRight} from "react-icons/bi"
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import OutsideClickHandler from "react-outside-click-handler";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Header = () => {
-
-  const [openMenu, setOpenMenu] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
 
   const getMeuStyles = (openMenu) => {
     if (document.documentElement.clientWidth <= 800) {
-      return {right: !openMenu && "-100%"}
+      return { right: !openMenu && "-100%" };
     }
-  }
+  };
 
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <a href="/">
-          <img src="./homefinderlogo.png" alt="logo" width={70} className="logo" />
+          <img
+            src="./homefinderlogo.png"
+            alt="logo"
+            width={70}
+            className="logo"
+          />
         </a>
-
-          <div className="flexCenter h-menu"
-          style={getMeuStyles(openMenu)}>
+        <OutsideClickHandler
+        onOutsideClick={()=>setOpenMenu(false)}
+        >
+          <div className="flexCenter h-menu" style={getMeuStyles(openMenu)}>
             <a href="">Homes</a>
             <a href="">Our Core Values</a>
             <a href="">Contact Us</a>
             <a href=""> Get Started</a>
             <button className="button">
-            <a href="">Contact</a>
+              <a href="">Contact</a>
             </button>
           </div>
-      <div className="menu-icon" onClick= {()=>setOpenMenu((prev)=>!prev)}>
-        <BiMenuAltRight size={30}/>
-      </div>
+        </OutsideClickHandler>
+        <div className="menu-icon" onClick={() => setOpenMenu((prev) => !prev)}>
+          <BiMenuAltRight size={30} />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
