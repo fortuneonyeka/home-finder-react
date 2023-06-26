5f6f26b965d723d6a515a26b85176a03fff0ea7a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-06-26 14:31:53 +0100

    contact responsive

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -7,83 +7,83 @@ import {HiChatBubbleBottomCenter} from "react-icons/hi2"
 const Contacts = () => {
   return (
     <section className="c-wrapper">
-      <div className="paddings innerWidth flexCenter c-container ">
-        <div className="flexColStart c-left">
-          <span className="orangeText">Our Contacts</span>
-          <span className="primaryText">Contact Us The Easy Way</span>
-          <span className="secondaryText">We have the needed Experience and Expertise To Help You find you THE place, a dream home</span>
+    <div className="paddings innerWidth flexCenter c-container ">
+      <div className="flexColStart c-left">
+        <span className="orangeText">Our Contacts</span>
+        <span className="primaryText">Contact Us The Easy Way</span>
+        <span className="secondaryText c-text">We have the needed Experience and Expertise To Help You find you THE place, a dream home</span>
 
 
-          <div className="flexColStart contactModes">
-            {/* first row */}
-              <div className="flexColStart row">
-                <div className="flexColCenter mode">
-                  <div className="flexStart">
-                    <div className="flexCenter icon">
-                      <MdCall size={25} />
-                    </div>
-                    <div className="flexColStart detail">
-                        <span className="primaryText">Call</span>
-                        <span className="secondaryText">+2348038852544</span>
-                    </div>
+        <div className="flexColStart contactModes">
+          {/* first row */}
+            <div className=" flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
                   </div>
-                  <div className="flexCenter button">Call Us Now</div>
-                </div>
-
-
-                <div className="flexColCenter mode">
-                  <div className="flexStart">
-                    <div className="flexCenter icon">
-                      <BsFillChatDotsFill size={25} />
-                    </div>
-                    <div className="flexColStart detail">
-                        <span className="primaryText">Chat</span>
-                        <span className="secondaryText">+2348038852544</span>
-                    </div>
+                  <div className="flexColStart detail">
+                      <span className="primaryText">Call</span>
+                      <span className="secondaryText">+2348038852544</span>
                   </div>
-                  <div className="flexCenter button">Chat Us Now</div>
                 </div>
+                <div className="flexCenter button">Call Us Now</div>
               </div>
-              {/* second row */}
-              
-              <div className="flexColStart row">
-                <div className="flexColCenter mode">
-                  <div className="flexStart">
-                    <div className="flexCenter icon">
-                      <BsFillChatDotsFill size={25} />
-                    </div>
-                    <div className="flexColStart detail">
-                        <span className="primaryText">Video Call</span>
-                        <span className="secondaryText">+2348038852544</span>
-                    </div>
-                  </div>
-                  <div className="flexCenter button">Video Call Us Now</div>
-                </div>
 
 
-                <div className="flexColCenter mode">
-                  <div className="flexStart">
-                    <div className="flexCenter icon">
-                      <HiChatBubbleBottomCenter size={25} />
-                    </div>
-                    <div className="flexColStart detail">
-                        <span className="primaryText">Email</span>
-                        <span className="secondaryText">ihedfortune@gmail.com</span>
-                    </div>
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
                   </div>
-                  <div className="flexCenter button">Email Us Now</div>
+                  <div className="flexColStart detail">
+                      <span className="primaryText">Chat</span>
+                      <span className="secondaryText">+2348038852544</span>
+                  </div>
                 </div>
+                <div className="flexCenter button">Chat Us Now</div>
               </div>
-          </div>
-        </div>
+            </div>
+            {/* second row */}
+            
+            <div className=" flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                      <span className="primaryText">Video Call</span>
+                      <span className="secondaryText">+2348038852544</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Video Call Us Now</div>
+              </div>
 
-        <div className="c-right">
-          <div className="image-container">
-            <img src="./contact.jpg" alt="" />
-          </div>
+
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                      <span className="primaryText">Email</span>
+                      <span className="secondaryText">ihedfortune@gmail.com</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Email Us Now</div>
+              </div>
+            </div>
         </div>
       </div>
-    </section>
+
+      <div className="c-right">
+        <div className="image-container">
+          <img src="./contact.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
   )
 }
 
