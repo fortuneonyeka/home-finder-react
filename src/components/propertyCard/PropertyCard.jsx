470217c9cbde9e9e84251f1ff2cd9741c2de4895
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-10 13:52:28 +0100

    route individual property through the parent properties route

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import {AiFillHeart} from "react-icons/ai"
 import "./propertyCard.css"
 import {truncate} from "lodash"
+import { useNavigate } from "react-router-dom"
 
 const PropertyCard = ({card}) => {
-  console.log(card.currency);
+  
+  const navigate = useNavigate()
   return (
-    <div className="flexColStart r-card">
+    <div className="flexColStart r-card" onClick={() =>navigate(`../properties/${card.id}`)}>
     <AiFillHeart size={24} color="gold"/>
     <img src={card.image} alt="home" />
     <span className="secondaryText r-price">
---
+++ src/pages/property/Property.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Property.css"
+
+const Property = () => {
+  return (
+    <div className="wrapper">Property</div>
+  )
+}
+
+export default Property
