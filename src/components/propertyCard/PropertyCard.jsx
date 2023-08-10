9acf1217c9f08d2ce94adc1bae23bc4db1399d02
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-10 12:53:23 +0100

    update with data from the backend and styling

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import {AiFillHeart} from "react-icons/ai"
 import "./propertyCard.css"
+import {truncate} from "lodash"
 
 const PropertyCard = ({card}) => {
   return (
     <div className="flexColStart r-card">
+    <AiFillHeart size={24} color="gold"/>
     <img src={card.image} alt="home" />
     <span className="secondaryText r-price">
-      <span style={{ color: "gold" }}>{card.currency}</span>
+      <span style={{ color: "blue" }}>${card.currency}</span>
       <span>{card.price}</span>
     </span>
-    <span className="primaryText">{card.name}</span>
-    <span className="secondaryText">{card.detail}</span>
+    <span className="primaryText">{truncate(card.title, {length: 15})}</span>
+    <span className="secondaryText">{truncate(card.description, {length:80})}</span>
   </div>
   )
 }
--- src/pages/properties/Properties.jsx
+++ src/pages/properties/Properties.jsx
@@ -1,11 +1,38 @@
 import React from "react";
-import Residencies from "../../components/residencies/Redidencies";
+import "./properties.css"
+import SeachBar from "../../components/searchBar/SeachBar";
+import useProperties from "../../hooks/useProperties";
+import { PuffLoader} from "react-spinners"
+import PropertyCard from "../../components/propertyCard/PropertyCard";
 
 const Properties = () => {
+  const {data, isError, isLoading} = useProperties()
+  if (isError) {
+    return(
+      <div className="wrapper">
+        <span>Error while fetching data</span>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return(
+      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+        <PuffLoader height="80" width="80" radius={1} color="#4066ff" aria-label="puff-loading "/>
+      </div>
+    )
+  }
   return (
     <div className="wrapper">
-      {/* <Residencies /> */}
-      Properties
+      <div className="flexColCenter paddings innerWidth properties-container">
+        <SeachBar />
+
+        <div className="paddings flexCenter properties">
+          {data.map((card,i) =>(
+            <PropertyCard card={card} key={i}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
