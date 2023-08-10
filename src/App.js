a166963a17dbfdf76e3b879772ad984d724897a2
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-10 12:53:55 +0100

    install and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import "./App.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
 import Layout from "./components/layout/Layout";
 import Website from "./pages/Website";
 import Properties from "./pages/properties/Properties";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-    <Suspense fallback={<div>Loading ....</div>}>
-
-      <Routes>
-        <Route element={<Layout />}>
-
-        <Route path="/" element={<Website />} />
-        <Route path="/properties" element={<Properties />} />
-        </Route>
-      </Routes>
-    </Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading ....</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <ToastContainer/>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
