f2f7ee1d0c1f8ba0722d48017a48eb8418d27833
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-10 12:52:40 +0100

    add routes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -7,12 +8,14 @@ const Footer = () => {
       <div className="paddings innerWidth flexCenter f-container">
         {/* left side */}
         <div className="flexColStart f-left">
+        <Link to="/">
           <img
             src="./homefinderlogo.png"
             alt="logo"
             width={90}
             className="logo"
-          />
+            />
+            </Link>
           <span className="secondaryText">
             We specialize in unlocking your dream home,
             <br />
@@ -26,7 +29,7 @@ const Footer = () => {
             G4 Road 12 Iceland Avenue, Banana Island, Lagos Nigeria
           </span>
           <div className="flexCenter f-menu">
-            <span><a href="#">Properties</a></span>
+            <span><Link to="/properties">Properties</Link></span>
             <span>Services</span>
             <span>Products</span>
             <span>About Us</span>
