d7b6f593bd0125a2fad218f67215e0f678ee7439
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-10 12:51:20 +0100

    update slider with data from the backend

--- src/components/residencies/Redidencies.jsx
+++ src/components/residencies/Redidencies.jsx
@@ -2,10 +2,29 @@ import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import "./Residencies.css";
-import data from "../../utils/slider.json";
+// import data from "../../utils/slider.json";
 import { sliderSettings } from "../../utils/common";
 import PropertyCard from "../propertyCard/PropertyCard";
+import useProperties from "../../hooks/useProperties";
+import { PulseLoader } from "react-spinners";
 const Redidencies = () => {
+  const {data, isError, isLoading} = useProperties()
+  if (isError) {
+    return(
+      <div className="wrapper">
+        <span>Error while fetching data</span>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return(
+      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+        <PulseLoader height="80" width="80" radius={1} color="#4066ff" aria-label="puff-loading "/>
+      </div>
+    )
+  }
+
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -16,7 +35,7 @@ const Redidencies = () => {
 
         <Swiper {...sliderSettings}>
           <SliderButtons />
-          {data.map((card, i) => (
+          {data.slice(0,8).map((card, i) => (
             <SwiperSlide key={i}>
              <PropertyCard card={card}/>
             </SwiperSlide>
