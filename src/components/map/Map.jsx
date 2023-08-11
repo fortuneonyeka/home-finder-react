1fe0b356bb6fc44398b2adef409ff9a6b9351384
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-11 13:42:50 +0100

    add leaflet map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer} from 'react-leaflet'
+import GeoCoderMarker from "../geoCoderMaker/GeoCoderMarker"
 
 const Map = ({address, city, country}) => {
   return (
-    <div>Map</div>
+    <MapContainer
+    center={[53.35, 18.8]} zoom={1} scrollWheelZoom={false}
+    style={{
+      height: "40vh", 
+      width:"100%", 
+      marginTop:"20px", 
+      zIndex:0,
+      }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+      <GeoCoderMarker  address={`${address} ${city} ${country}`}/>
+    </MapContainer>
   )
 }
 
