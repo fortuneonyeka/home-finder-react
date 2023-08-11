8853dcdde7006985c2ae4c56d13ee38281d87155
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-11 14:27:34 +0100

    debugging map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { MapContainer, TileLayer} from 'react-leaflet'
+import {MapContainer, TileLayer} from 'react-leaflet'
 import GeoCoderMarker from "../geoCoderMaker/GeoCoderMarker"
-
 const Map = ({address, city, country}) => {
   return (
     <MapContainer
-    center={[53.35, 18.8]} zoom={1} scrollWheelZoom={false}
+    center={[53.35, 18.8]}
+    zoom={1}
+    scrollWheelZoom={false}
     style={{
-      height: "40vh", 
-      width:"100%", 
-      marginTop:"20px", 
-      zIndex:0,
-      }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-      <GeoCoderMarker  address={`${address} ${city} ${country}`}/>
+        height: "40vh",
+        width: "100%",
+        marginTop: "20px",
+        zIndex: 0,
+       
+    }}
+    >
+        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+        <GeoCoderMarker address={`${address} ${city} ${country}`} />
     </MapContainer>
   )
 }
