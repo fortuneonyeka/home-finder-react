a01a5623c4277d74ec96e84ca9cc8168293d2340
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-14 13:03:48 +0100

    add login with auth0,mantine and user profile. Conditional render login button or user profile and mantine library for drop down and logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,13 @@ import useHeaderColor from "../../hooks/useHeaderColor";
 import { Link, NavLink } from "react-router-dom";
 import OutsideClickHandler from "react-outside-click-handler";
 import { BiMenuAltRight } from "react-icons/bi";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProfileMenu from "../profileMenu/ProfileMenu";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const headerColor = useHeaderColor();
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
   const getMeuStyles = (openMenu) => {
     if (document.documentElement.clientWidth <= 800) {
@@ -26,17 +29,18 @@ const Header = () => {
             className="logo"
           />
         </Link>
-        <OutsideClickHandler
-        onOutsideClick={()=>setOpenMenu(false)}
-        >
+        <OutsideClickHandler onOutsideClick={() => setOpenMenu(false)}>
           <div className="flexCenter h-menu" style={getMeuStyles(openMenu)}>
-            
             <NavLink to="/properties">Properties</NavLink>
-           
-              <a href="mailto:ihedfortune@gmail.com">Contact</a>
 
-              <button className="button">Login</button>
-          
+            <a href="mailto:ihedfortune@gmail.com">Contact</a>
+
+            {
+              !isAuthenticated ?
+              <button className="button" onClick={loginWithRedirect}>
+                Login
+              </button> : <ProfileMenu user={user} logout={logout}/>
+            }
           </div>
         </OutsideClickHandler>
         <div className="menu-icon" onClick={() => setOpenMenu((prev) => !prev)}>
---
+++ src/components/profileMenu/ProfileMenu.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Menu, Button, Avatar, Text } from '@mantine/core';
+
+const ProfileMenu = ({user, logout}) => {
+  return (
+    <Menu>
+       <Menu.Target>
+        <Avatar src={user?.picture} alt="user image" radius={"xl"}/>
+      </Menu.Target>
+      <Menu.Dropdown>
+          <Menu.Item>Favorites</Menu.Item>
+          <Menu.Item>Bookings</Menu.Item>
+          <Menu.Item onClick={()=>{
+            localStorage.clear();
+            logout()
+          }}>Logout</Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  )
+}
+
+export default ProfileMenu
