ce38f42e25cb7af91b0d9a7ecc87bb35f839fff8
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-14 12:18:17 +0100

    setup auth0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-1l1xwyeleiwmn68i.us.auth0.com"
+    clientId="IIXXvYrS1CJiGKnk9rjROqmscl0QCiki"
+    authorizationParams={{
+      redirect_uri:"http://localhost:3000"
+    }}
+    audience = "http://localhost:4996"
+    scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
-
