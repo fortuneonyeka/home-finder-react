f73291178b1c8b4418cbdb8a19cf50e0b1ef687d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-16 13:09:18 +0100

    removed unused

--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import Companies from "../components/companies/Companies";
 import Redidencies from "../components/residencies/Redidencies";
 import Value from "../components/values/Value";
 import Contacts from "../components/contacts/Contacts";
 import GetStarted from "../components/getStarted/GetStarted";
-import Footer from "../components/footer/Footer";
 
 const Website = () => {
   return (
