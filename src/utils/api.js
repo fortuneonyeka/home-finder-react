f729e27f453da1540f678e52fec3cfa249fccaf8
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-18 11:49:21 +0100

    createUser function that accepts jwt token

--- src/utils/api.js
+++ src/utils/api.js
@@ -36,9 +36,13 @@ return response.data
   }
 }
 
-export const createUser = async(email) => {
+export const createUser = async(email, token) => {
   try {
-    await api.post(`/user/register`, {email})
+    await api.post(`/user/register`, {email}, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
   } catch (error) {
     toast.error("Something went wrong, Please try again")
     throw error
